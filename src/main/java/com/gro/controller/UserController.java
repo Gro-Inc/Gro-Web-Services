ed3767c610ecd15ef583d14d30afd36f808335a0
Conor <ckosidowski11@winona.edu>
2017-02-25 16:35:37 -0600

    Added a simple registration interface * Doesn't encrypt the password * The interface is ugly

--- src/main/java/com/gro/controller/UserController.java
+++ src/main/java/com/gro/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.gro.controller;
 
-import com.gro.model.DTO.RESTResponse;
 import com.gro.model.constants.Table;
 import com.gro.model.database.GroDBInterface;
 import com.mongodb.BasicDBObject;
@@ -9,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
@@ -16,11 +16,16 @@ public class UserController {
     @Autowired
     private GroDBInterface groDBInterface;
 
+    @ResponseBody
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public void registerUser(@RequestParam final String username) {
+    public void registerUser(
+            @RequestParam final String username,
+            @RequestParam final String password
+    ) {
         final BasicDBObject userDoc = new BasicDBObject();
 
         userDoc.put("username", username);
+        userDoc.put("password", password);
 
         groDBInterface.getTable(Table.USERS).insert(userDoc);
     }
